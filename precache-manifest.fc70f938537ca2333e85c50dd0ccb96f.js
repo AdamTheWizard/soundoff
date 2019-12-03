afacd6e905ec434b31bb3d4c1ddb068b1ed46dca
Adam Wall <a.d.a.m.io@hotmail.com>
2019-12-03 23:52:00 +0000

    Updates

---
+++ precache-manifest.fc70f938537ca2333e85c50dd0ccb96f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa05fd28f189cd2c77841d9299b93bf",
+    "url": "/soundoff/index.html"
+  },
+  {
+    "revision": "ca7d5ff26ddd8df5c3df",
+    "url": "/soundoff/static/css/main.5921719a.chunk.css"
+  },
+  {
+    "revision": "35feaf785d8630216271",
+    "url": "/soundoff/static/js/2.fe49e526.chunk.js"
+  },
+  {
+    "revision": "ca7d5ff26ddd8df5c3df",
+    "url": "/soundoff/static/js/main.e56c67f2.chunk.js"
+  },
+  {
+    "revision": "046036a2d08b5f1aef34",
+    "url": "/soundoff/static/js/runtime-main.b0eb291b.js"
+  }
+]);
